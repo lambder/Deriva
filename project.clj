7ffd29fa8978fcec42eb0b5bf0f385b8c9f6a39e
Daniel Kwiecinski <daniel@lambder.com>
2013-10-01 11:58:52 +0100

    added scm to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject deriva "0.1.0-SNAPSHOT"
   :description "Automatic Differentiation for Java and Clojure"
-  :url "http://lambder.com"
+  :url "http://lambder.com"  
+  :scm {:url "git@github.com:lambder/Deriva.git"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
